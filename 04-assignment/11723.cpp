6954c7f265d25b05778324001309adc29c0a9e0e
hyunsu999 <luna1863@ewha.ac.kr>
2026-04-08 23:08:36 +0900

    [브루트포스] 4월 8일

---
+++ 04-assignment/11723.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int M;
+    cin >> M;
+
+    int arr[21] = { 0 };
+
+    for (int i = 0; i < M; i++) {
+        string cmd;
+        cin >> cmd;
+
+        if (cmd == "add") {
+            int x;
+            cin >> x;
+            arr[x] = 1;
+        }
+        else if (cmd == "remove") {
+            int x;
+            cin >> x;
+            arr[x] = 0;
+        }
+        else if (cmd == "check") {
+            int x;
+            cin >> x;
+            cout << arr[x] << '\n';
+        }
+        else if (cmd == "toggle") {
+            int x;
+            cin >> x;
+            if (arr[x] == 1) arr[x] = 0;
+            else arr[x] = 1;
+        }
+        else if (cmd == "all") {
+            for (int j = 1; j <= 20; j++) {
+                arr[j] = 1;
+            }
+        }
+        else if (cmd == "empty") {
+            for (int j = 1; j <= 20; j++) {
+                arr[j] = 0;
+            }
+        }
+    }
+
+    return 0;
+}
